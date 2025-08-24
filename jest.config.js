19ee6d4d006a4edf992a45b389ec737b8182c03d
Soumen sarkar <soumensarkar7498@gmail.com>
2025-08-25 01:10:07 +0530

    Remove root .babelrc to keep SWC enabled

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,14 @@
-module.exports = {
-    testEnvironment: "jest-environment-jsdom",
-    moduleFileExtensions: ["js", "jsx"],
-    moduleDirectories: ["node_modules", "<rootDir>/"],
-    setupFilesAfterEnv: ["@testing-library/jest-dom"], 
-  };
-  
+
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({ dir: './' })
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // optional: ignore E2E or build outputs
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+}
+
+module.exports = createJestConfig(customJestConfig)
